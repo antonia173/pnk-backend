c4e3664fa9de97d4821f853d4369d5d8e982d2cc
ab170301 <antonia.blazevic173@gmail.com>
2023-05-21 18:10:25 +0200

    cors fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,46 +1,51 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4200' 
+    origins '*' 
 
-    resource '/real-estates',
+    resource '*',
       headers: :any,
-      methods: [:get, :options]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+
+    # resource '/real-estates',
+    #   headers: :any,
+    #   methods: [:get, :options]
   
-    resource '/real-estates/*',
-      headers: :any,
-      methods: [:get, :options]
+    # resource '/real-estates/*',
+    #   headers: :any,
+    #   methods: [:get, :options]
 
-    resource '/real-estates/add',
-      headers: :any,
-      methods: [:post, :options]
+    # resource '/real-estates/add',
+    #   headers: :any,
+    #   methods: [:post, :options]
 
-    resource '/real-estates/update/*',
-      headers: :any,
-      methods: [:put, :options]
+    # resource '/real-estates/update/*',
+    #   headers: :any,
+    #   methods: [:put, :options]
 
-    resource '/real-estates/delete/*',
-      headers: :any,
-      methods: [:delete, :options]
+    # resource '/real-estates/delete/*',
+    #   headers: :any,
+    #   methods: [:delete, :options]
 
-    resource '/real-estates/content/*',
-      headers: :any,
-      methods: [:get, :options]
+    # resource '/real-estates/content/*',
+    #   headers: :any,
+    #   methods: [:get, :options]
 
-    resource '/real-estates/types',
-      headers: :any,
-      methods: [:get, :options]
+    # resource '/real-estates/types',
+    #   headers: :any,
+    #   methods: [:get, :options]
 
-    resource '/real-estates/types/add',
-      headers: :any,
-      methods: [:post, :options]
+    # resource '/real-estates/types/add',
+    #   headers: :any,
+    #   methods: [:post, :options]
 
-    resource '/real-estates/types/update/*',
-      headers: :any,
-      methods: [:put, :options]
+    # resource '/real-estates/types/update/*',
+    #   headers: :any,
+    #   methods: [:put, :options]
 
-    resource '/real-estates/types/delete/*',
-      headers: :any,
-      methods: [:delete, :options]
+    # resource '/real-estates/types/delete/*',
+    #   headers: :any,
+    #   methods: [:delete, :options]
       
   end
 end
